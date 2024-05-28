b59805cf09ff1f743f58a649ce07f58b2404d990
therealtub <HappytrelikalEmmitt@outlook.com>
2024-05-27 21:31:51 -0400

    Added regions, to enhance the neatness of the code.

--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,14 @@
 #include "pros/abstract_motor.hpp"
 #include "pros/motor_group.hpp"
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-
-/*
-Red Motor - 100 RPM - pros::MotorGearset::red
-Green Motor - 200 RPM - pros::MotorGearset::green
-Blue Motor - 600 RPM - pros::MotorGearset::blue
-*/
-
 // Motor configuration
+#pragma region Motor Configuration
 pros::MotorGroup left_motors({1, 2, 3}, pros::MotorGearset::blue);
 pros::MotorGroup right_motors({4, 5, 6}, pros::MotorGearset::blue);
+#pragma endregion
 
-// drivetrain settings
+// Drivetrain settings
+#pragma region Drivetrain Settings
 lemlib::Drivetrain drivetrain(&left_motors, // left motor group
                               &right_motors, // right motor group
                               10, // 10 inch track width
@@ -29,7 +19,10 @@ lemlib::Drivetrain drivetrain(&left_motors, // left motor group
                               1200, // drivetrain rpm is 360
                               2 // horizontal drift is 2 (for now)
 );
+#pragma endregion
 
+// Sensors
+#pragma region Sensors
 // create an imu on port 10
 pros::Imu imu(7);
 
@@ -49,7 +42,10 @@ lemlib::OdomSensors sensors(&vertical_tracking_wheel, // vertical tracking wheel
                             nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
                             &imu // inertial sensor
 );
+#pragma endregion
 
+// PID controllers
+#pragma region PID Controllers
 // lateral PID controller
 lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
                                               0, // integral gain (kI)
@@ -73,15 +69,19 @@ lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
                                               500, // large error range timeout, in milliseconds
                                               0 // maximum acceleration (slew)
 );
+#pragma endregion
 
-
-// create the chassis
+// Create the chassis
+#pragma region Chassis
 lemlib::Chassis chassis(drivetrain, // drivetrain settings
                         lateral_controller, // lateral PID settings
                         angular_controller, // angular PID settings
                         sensors // odometry sensors
 );
+#pragma endregion
 
+// Initialization
+#pragma region Initialization
 void initialize() {
     pros::lcd::initialize(); // initialize brain screen
     chassis.calibrate(); // calibrate sensors
@@ -97,35 +97,20 @@ void initialize() {
         }
     });
 }
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
+#pragma endregion
+
+// Disabled
+#pragma region Disabled
 void disabled() {}
+#pragma endregion
 
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
+// Competition Initialize
+#pragma region Competition Initialize
 void competition_initialize() {}
+#pragma endregion
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
+// Autonomous
+#pragma region Autonomous
 // path file name is "example.txt".
 // "." is replaced with "_" to overcome c++ limitations
 ASSET(skillspath_txt);
@@ -137,20 +122,10 @@ void autonomous() {
     // timeout: 2000 ms
     chassis.follow(skillspath_txt, 15, 2000);
 }
+#pragma endregion
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
+// Operator Control
+#pragma region Operator Control
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 void opcontrol() {
@@ -167,3 +142,4 @@ void opcontrol() {
         pros::delay(25);
     }
 }
+#pragma endregion
