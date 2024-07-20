7ae665e414863b284b73b4be0cddd60fd479150a
therealtub <HappytrelikalEmmitt@outlook.com>
2024-07-20 02:14:18 -0400

    Added updated PID values.

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,22 @@
 #include "lemlib/chassis/trackingWheel.hpp"
 #include "pros/abstract_motor.hpp"
 #include "pros/motor_group.hpp"
+#include "pros/rtos.hpp"
+#include <iostream>
 
 // Motor configuration
 #pragma region Motor Configuration
-pros::MotorGroup left_motors({1, 2, 3}, pros::MotorGearset::blue);
-pros::MotorGroup right_motors({4, 5, 6}, pros::MotorGearset::blue);
+pros::MotorGroup left_motors({-1,-2, -3}, pros::MotorGearset::blue);
+pros::MotorGroup right_motors({18, 19, 20}, pros::MotorGearset::blue);
 #pragma endregion
 
 // Drivetrain settings
 #pragma region Drivetrain Settings
 lemlib::Drivetrain drivetrain(&left_motors, // left motor group
                               &right_motors, // right motor group
-                              10, // 10 inch track width
+                              15, // 10 inch track width
                               lemlib::Omniwheel::NEW_325, // using new 4" omnis
-                              1200, // drivetrain rpm is 360
+                              450, // drivetrain rpm is 360
                               2 // horizontal drift is 2 (for now)
 );
 #pragma endregion
@@ -24,17 +26,17 @@ lemlib::Drivetrain drivetrain(&left_motors, // left motor group
 // Sensors
 #pragma region Sensors
 // create an imu on port 10
-pros::Imu imu(7);
+pros::Imu imu(4);
 
 // create a v5 rotation sensor on port 1
-pros::Rotation horizontal_sensor(8);
+pros::Rotation horizontal_sensor(9);
 // create a v5 rotation sensor on port 1
-pros::Rotation vertical_sensor(9);
+pros::Rotation vertical_sensor(17);
 
 // horizontal tracking wheel
-lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_sensor, lemlib::Omniwheel::NEW_325, -5.75);
+lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_sensor, lemlib::Omniwheel::NEW_275, -1);
 // vertical tracking wheel
-lemlib::TrackingWheel vertical_tracking_wheel(&vertical_sensor, lemlib::Omniwheel::NEW_325, -2.5);
+lemlib::TrackingWheel vertical_tracking_wheel(&vertical_sensor, lemlib::Omniwheel::NEW_275, -7);
 
 lemlib::OdomSensors sensors(&vertical_tracking_wheel, // vertical tracking wheel 1, set to null
                             nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
@@ -59,18 +61,17 @@ lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
 );
 
 // angular PID controller
-lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+lemlib::ControllerSettings angular_controller(4, // proportional gain (kP)
                                               0, // integral gain (kI)
                                               10, // derivative gain (kD)
                                               3, // anti windup
-                                              1, // small error range, in degrees
+                                              1, // small error range, in inches
                                               100, // small error range timeout, in milliseconds
-                                              3, // large error range, in degrees
+                                              3, // large error range, in inches
                                               500, // large error range timeout, in milliseconds
                                               0 // maximum acceleration (slew)
 );
 #pragma endregion
-
 // Create the chassis
 #pragma region Chassis
 lemlib::Chassis chassis(drivetrain, // drivetrain settings
@@ -92,6 +93,8 @@ void initialize() {
             pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
             pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
             pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            pros::lcd::print(11, "Horizontal Wheel Position: %f", horizontal_sensor.get_position());
+            pros::lcd::print(12, "Vertical Wheel Position: %f", vertical_sensor.get_position());
             // delay to save resources
             pros::delay(20);
         }
@@ -115,33 +118,14 @@ void competition_initialize() {}
 // "." is replaced with "_" to overcome c++ limitations
 ASSET(skillspath_txt);
 
+void logPosition(const std::string& positionName, const std::string& command, const lemlib::Pose& pose) {
+    std::cout << positionName << " - Command: " << command << " - X: " << pose.x << ", Y: " << pose.y << ", Theta: " << pose.theta << std::endl;
+}
+
 void autonomous() {
     // set chassis pose
-    chassis.setPose(0, 0, 0);
-    chassis.moveToPoint(-0.097, -18.491, 5000);
-    chassis.moveToPoint(-0.139, -42.31, 5000);
-    chassis.moveToPoint(23.61, -42.392, 5000);
-    chassis.moveToPoint(23.449, -18.538, 5000);
-    chassis.moveToPoint(23.411, -6.683, 5000);
-    chassis.moveToPoint(35.553, -18.668, 5000);
-    chassis.moveToPoint(41.324, -0.272, 5000);
-    chassis.moveToPoint(35.551, -65.796, 5000);
-    chassis.moveToPoint(26.466, -90.417, 5000);
-    chassis.moveToPoint(-23.793, -113.209, 5000);
-    chassis.moveToPoint(0.147, -88.931, 5000);
-    chassis.moveToPoint(-46.822, -89.245, 5000);
-    chassis.moveToPoint(-71.022, -89.516, 5000);
-    chassis.moveToPoint(-82.466, -65.884, 5000);
-    chassis.moveToPoint(-88.542, -130.899, 5000);
-    chassis.moveToPoint(-70.672, -41.904, 5000);
-    chassis.moveToPoint(-47.335, -41.904, 5000);
-    chassis.moveToPoint(-47.354, -18.547, 5000);
-    chassis.moveToPoint(-70.987, -18.216, 5000);
-    chassis.moveToPoint(-70.994, -6.682, 5000);
-    chassis.moveToPoint(-82.406, -18.647, 5000);
-    chassis.moveToPoint(-88.572, -0.977, 5000);
-    chassis.moveToPoint(-34.111, -52.982, 5000);
-}
+    chassis.follow("static\real.txt", 15, 5000);
+}   
 #pragma endregion
 
 // Operator Control
